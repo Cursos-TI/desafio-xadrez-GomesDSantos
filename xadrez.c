7ab4017fbd467bbf7cad13a3ec127cd8be6cb8f9
Gabriel Gomes <gabrielgomes.santos013@gmail.com>
2025-05-31 21:11:04 -0300

    Adição das funcionalidades do nivel Aventureiro Xadrez
    Adição da funcionalidade de movimentação da peça cavalo, correção de sintaxe e correção de funcionalidades incompletas do nivel iniciante

--- xadrez.c
+++ xadrez.c
@@ -2,12 +2,11 @@
 
 int main(void) {
 
-
-    int bispoX,bispoY,rainhaX,rainhaY,torreX,torreY,cont, escolha; //VARIAVEIS DO PROJETO
+    int bispoX,bispoY,rainhaX,rainhaY,torreX,torreY,cavaloX,cavaloY,cont,escolha; //VARIAVEIS DO PROJETO
     do {
         //DEFINIÇÃO DA ESCOLHA DE MOVIMENTAÇÃO DA PEÇA
         printf("QUAL PECA DESEJA MOVIMENTAR?\n\n");
-        printf("1.Bispo\n2.Torre\n3.Rainha\n4.Sair\n\n");
+        printf("1.Bispo\n2.Torre\n3.Rainha\n4.Cavalo\n5.sair\n\n");
         scanf("%i",&escolha);
         printf("\n");
 
@@ -17,10 +16,17 @@ int main(void) {
                 bispoX,bispoY = 0;
                 cont = 1;
                 while (cont != 6) {
-                    printf("POSICAO BISPO (%i): X - %i / Y - %i\n", cont, bispoX, bispoY);
-                    bispoX++;
-                    bispoY++;
-                    cont++;
+                    if (cont == 1) {
+                        printf("POSICAO BISPO (%i): X - %i / Y - %i = Padrao\n", cont, bispoX, bispoY);
+                        cont++;
+                    }
+                    else if (cont != 6) {
+                        bispoX++;
+                        printf("POSICAO BISPO (%i): X - %i / Y - %i = Direita\n", cont, bispoX, bispoY);
+                        bispoY++;
+                        printf("POSICAO BISPO (%i): X - %i / Y - %i = Cima\n", cont, bispoX, bispoY);
+                        cont++;
+                    }
                 }
                 printf("\n");
                 break;
@@ -30,9 +36,15 @@ int main(void) {
                 torreY = 0;
                 cont = 1;
                 while (cont != 6) {
-                    printf("POSICAO TORRE (%i): X - %i / Y - %i\n", cont, torreX, torreY);
-                    torreX++;
-                    cont++;
+                    if (cont == 1) {
+                        printf("POSICAO TORRE (%i): X - %i / Y - %i = Padrao\n", cont, torreX, torreY);
+                        cont++;
+                    }
+                    else if (cont != 6) {
+                        torreX++;
+                        printf("POSICAO TORRE (%i): X - %i / Y - %i = Direita\n", cont, torreX, torreY);
+                        cont++;
+                    }
                 }
                 printf("\n");
                 break;
@@ -40,20 +52,48 @@ int main(void) {
             case 3: { //CASOS DE MOVIMENTAÇÃO: RAINHA
                 rainhaX = 8;
                 rainhaY = 8;
-                for (cont = 1; cont <= 8; cont++) {
-                    printf("POSICAO RAINHA (%i): X - %i / Y - %i\n", cont, rainhaX, rainhaY);
-                    rainhaX--;
+                for (cont = 1; cont <= 9; cont++) {
+                    if (cont == 1) {
+                        printf("POSICAO RAINHA (%i): X - %i / Y - %i = Padrao\n", cont, rainhaX, rainhaY);
+                    }
+                    else if (cont != 9) {
+                        rainhaX--;
+                        printf("POSICAO RAINHA (%i): X - %i / Y - %i = Esquerda\n", cont, rainhaX, rainhaY);
+                    }
                 }
                 printf("\n");
                 break;
             }
-            case 4: { //CASOS DE MOVIMENTAÇÃO: SAIDA DO PROGRAMA
+            case 4: {
+                cavaloX = 5;
+                cavaloY = 5;
+                for (cont = 1; cont <= 5; cont++) {
+                    if (cont == 1) {
+                        printf("POSICAO CAVALO (%i): X - %i / Y - %i = Padrao\n", cont, cavaloX, cavaloY);
+                    }
+                    else if (cont <= 3) {
+                        cavaloY--;
+                        printf("POSICAO CAVALO (%i): X - %i / Y - %i = Baixo\n", cont, cavaloX, cavaloY);
+                    }
+                    else if (cont > 3) {
+                        cavaloX--;
+                        printf("POSICAO CAVALO (%i): X - %i / Y - %i = Esquerda\n", cont, cavaloX, cavaloY);
+                        cont++;
+                    }
+                }
+                printf("\n");
+                break;
+            }
+
+            case 5: { //CASOS DE MOVIMENTAÇÃO: SAIDA DO PROGRAMA
                 printf("Obrigado por jogar!");
                 printf("\n");
                 break;
             }
-            default: //CASOS DE MOVIMENTAÇÃO: OPCAO INVALIDA
-                printf("Esse opcao nao e valida");
+            default: { //CASOS DE MOVIMENTAÇÃO: OPCAO INVALIDA
+                printf("Esse opcao nao e valida\n");
+                break;
+            }
         }
-    } while (escolha != 4);
+    } while (escolha != 5);
 }
